ad5fcbafa58cd8c55dd4104bee218b670cad4ffd
Tejas VM <veeragantimanjunathtejas@gmail.com>
2023-09-19 18:04:10 -0400

    Resolved parallel processing issue in Container program

--- Container/Container.cs
+++ Container/Container.cs
@@ -1,10 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 namespace DeveloperSample.Container
 {
     public class Container
     {
-        public void Bind(Type interfaceType, Type implementationType) => throw new NotImplementedException();
-        public T Get<T>() => throw new NotImplementedException();
+        private Dictionary<Type, Type> typeRegistry = new();
+
+        public void Bind(Type interfaceType, Type implementationType)
+        {
+            typeRegistry[interfaceType] = implementationType;
+        }
+
+        public T Get<T>()
+        {
+            Type implementationType = typeRegistry[typeof(T)];
+            return (T)Activator.CreateInstance(implementationType);
+        }
     }
 }
