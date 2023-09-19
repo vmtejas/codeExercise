bcddee6b208dce0da3e6444582b8bf65ceacfaf3
Tejas VM <veeragantimanjunathtejas@gmail.com>
2023-09-19 18:26:50 -0400

    Resolved async and await issue in SyncDebug program

--- Syncing/SyncDebug.cs
+++ Syncing/SyncDebug.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace DeveloperSample.Syncing
@@ -12,13 +11,17 @@ namespace DeveloperSample.Syncing
         public List<string> InitializeList(IEnumerable<string> items)
         {
             var bag = new ConcurrentBag<string>();
-            Parallel.ForEach(items, async i =>
+
+            var tasks = items.Select(i =>
             {
-                var r = await Task.Run(() => i).ConfigureAwait(false);
-                bag.Add(r);
+                return Task.Run(() =>
+                {
+                    bag.Add(i);
+                });
             });
-            var list = bag.ToList();
-            return list;
+
+            Task.WaitAll(tasks.ToArray());
+            return bag.ToList();
         }
 
         public Dictionary<int, string> InitializeDictionary(Func<int, string> getItem)
@@ -26,23 +29,12 @@ namespace DeveloperSample.Syncing
             var itemsToInitialize = Enumerable.Range(0, 100).ToList();
 
             var concurrentDictionary = new ConcurrentDictionary<int, string>();
-            var threads = Enumerable.Range(0, 3)
-                .Select(i => new Thread(() => {
-                    foreach (var item in itemsToInitialize)
-                    {
-                        concurrentDictionary.AddOrUpdate(item, getItem, (_, s) => s);
-                    }
-                }))
-                .ToList();
 
-            foreach (var thread in threads)
+            Parallel.ForEach(itemsToInitialize, item =>
             {
-                thread.Start();
-            }
-            foreach (var thread in threads)
-            {
-                thread.Join();
-            }
+                var value = getItem(item);
+                concurrentDictionary.TryAdd(item, value);
+            });
 
             return concurrentDictionary.ToDictionary(kv => kv.Key, kv => kv.Value);
         }
