6d5530c56f553f852662c4814615e967c3dbdbd2
Tejas VM <veeragantimanjunathtejas@gmail.com>
2023-09-22 13:56:28 -0400

    Replaced gitignore

--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -9,6 +9,9 @@ namespace DeveloperSample.Algorithms
             if (n < 0)
                 throw new ArgumentException("Input must be a non-negative integer");
 
+            if (n > 12)
+                throw new ArgumentException("Input is large and must be below 12");
+
             int factorialResult = 1;
             for (int i = 1; i <= n; i++)
             {
@@ -17,17 +20,24 @@ namespace DeveloperSample.Algorithms
             return factorialResult;
         }
 
-        public static string FormatSeparators(params string[] items)
+        //public static string FormatSeparators(params string[] items)
+        //{
+        //    if (items == null || items.Length == 0)
+        //        return string.Empty;
+
+        //    if (items.Length == 1)
+        //        return items[0];
+
+        //    string stringResult = string.Join(", ", items, 0, items.Length - 1);
+        //    stringResult += " and " + items[^1];
+        //    return stringResult;
+        //}
+
+        public static string FormatSeparators(params string[] items) => items switch
         {
-            if (items == null || items.Length == 0)
-                return string.Empty;
-
-            if (items.Length == 1)
-                return items[0];
-
-            string stringResult = string.Join(", ", items, 0, items.Length - 1);
-            stringResult += " and " + items[items.Length - 1];
-            return stringResult;
-        }
+            [var x] => x,
+            [.., var x] => $"{string.Join(", ", items, 0, items.Length - 1)} and {x}",
+            _ => (string.Empty)
+        };
     }
 }
--- Frontend/login_app/src/LoginForm.js
+++ Frontend/login_app/src/LoginForm.js
@@ -7,9 +7,25 @@ import './LoginForm.css';
 const LoginForm = (props) => {
     const [login, setLogin] = useState('');
     const [password, setPassword] = useState('');
+    const [loginError, setLoginError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        if (!login) {
+            setLoginError("Please enter a username!");
+            return;
+        }
+        if (!password) {
+            setPasswordError("Please enter a password!");
+            return;
+        }
+        if (password.length < 6) {
+            setPasswordError("Password should be at least 6 characters long!");
+            return;
+        }
+        setLoginError('');
+        setPasswordError('');
         props.onSubmit({ login, password });
     }
 
@@ -21,9 +37,11 @@ const LoginForm = (props) => {
                 </Typography>
                 <div className="form-group">
                     <TextBox label="Name" id="name" value={login} size="small" onChange={(e) => setLogin(e.target.value)} />
+                    {loginError && <div className="error-message">{loginError}</div>}
                 </div>
                 <div className="form-group">
                     <TextBox label="Password" type="password" id="password" value={password} size="small" onChange={(e) => setPassword(e.target.value)} />
+                    {passwordError && <div className="error-message">{passwordError}</div>}
                 </div>
                 <div className="form-group">
                     <Button type="submit">
