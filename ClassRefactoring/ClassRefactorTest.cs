e58db7c28990cb5124c3017909dcc360deabab9b
Tejas VM <veeragantimanjunathtejas@gmail.com>
2023-09-26 19:29:24 -0400

    Added more test cases  to ClassRefactor program

--- ClassRefactoring/ClassRefactorTest.cs
+++ ClassRefactoring/ClassRefactorTest.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit;
 
 namespace DeveloperSample.ClassRefactoring
@@ -36,6 +37,37 @@ namespace DeveloperSample.ClassRefactoring
             var swallow = swallowFactory.GetSwallow(SwallowType.European);
             swallow.ApplyLoad(SwallowLoad.Coconut);
             Assert.Equal(16, swallow.GetAirspeedVelocity());
+        }
+
+        [Fact]
+        public void ThrowExceptionForInvalidSwallowType()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Swallow((SwallowType)999));
+        }
+
+        [Fact]
+        public void ThrowExceptionForInvalidLoadType()
+        {
+            var swallow = new Swallow(SwallowType.African);
+            Assert.Throws<ArgumentOutOfRangeException>(() => swallow.ApplyLoad((SwallowLoad)999));
+        }
+
+        [Fact]
+        public void ResettingSwallowLoadToNone()
+        {
+            var swallow = new Swallow(SwallowType.European);
+            swallow.ApplyLoad(SwallowLoad.Coconut);
+            swallow.ApplyLoad(SwallowLoad.None);
+            Assert.Equal(20, swallow.GetAirspeedVelocity());
+        }
+
+        [Fact]
+        public void ApplyingLoadToAlreadyLoadedSwallow()
+        {
+            var swallow = new Swallow(SwallowType.African);
+            swallow.ApplyLoad(SwallowLoad.Coconut);
+            swallow.ApplyLoad(SwallowLoad.Coconut);
+            Assert.Equal(18, swallow.GetAirspeedVelocity());
         }
     }
 }
